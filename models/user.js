84498110a6c586d1d8c4f8b22b18797444e4f836
Daniel Gómez <danielgomezv2@gmail.com>
2022-11-04 10:42:49 -0300

    user model modified

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 const { Schema, model } = require('mongoose');
 
-const UsuarioSchema = Schema({
+const UserSchema = Schema({
     username: { type: String, required: [true, 'El nombre de usuario es obligatorio'], unique: true },
     email: { type: String, required: [true, 'El email es obligatorio'], unique: true },
     password: { type: String, required: [true, 'El password es obligatorio'] }
 });
 
 
-UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, _id, ...usuario } = this.toObject();
-    usuario.uid = _id;
-    return usuario;
+UserSchema.methods.toJSON = function() {
+    const { __v, password, _id, ...user } = this.toObject();
+    user.uid = _id;
+    return user;
 };
 
-module.exports = model('Usuario', UsuarioSchema);
+module.exports = model('User', UserSchema);
